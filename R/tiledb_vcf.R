1c7e0154d0d6c1a1ca8ea52ab05b1fd01d514a05
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-09-14 17:58:14 +0400

    flags

--- R/tiledb_vcf.R
+++ R/tiledb_vcf.R
@@ -21,3 +21,89 @@ tiledb_vcf_version <- function() {
 tiledb_vcf_available <- function() {
   .Call(RC_tiledb_vcf_available, PACKAGE = "Rlibtiledbvcf")
 }
+
+#' Get the TileDB-VCF Library Path
+#'
+#' Returns the path to the TileDB-VCF libraries installed with the Rlibtiledbvcf package.
+#'
+#' @return A character string containing the TileDB-VCF library path.
+#' @export
+tiledb_vcf_library_path <- function() {
+  libs_path <- system.file(
+    "TileDBVCF",
+    "lib",
+    package = "Rlibtiledbvcf"
+  )
+  return(libs_path)
+}
+
+#' Get TileDB-VCF Library Items
+#' Returns the full path to the library items required to link against the TileDB-VCF libraries
+#' @return A character string containing the library items.
+#' @export
+tiledb_vcf_library_items <- function() {
+  libs_path <- tiledb_vcf_library_path()
+  items <- list.files(libs_path, full.names = TRUE)
+  items <- items[grepl(.Platform$dynlib.ext, items, fixed = TRUE)] |>
+    normalizePath() |>
+    paste0(collapse = " ")
+  return(items)
+}
+
+#' Get TileDB-VCF Include Path
+#' Returns the include path required to compile code that uses the TileDB-VCF library.
+#' @return A character string containing the include path.
+#' @export
+tiledb_vcf_include_path <- function() {
+  include_path <- system.file(
+    "TileDBVCF",
+    "include",
+    package = "Rlibtiledbvcf"
+  )
+  return(include_path)
+}
+
+#' Get TileDB-VCF Include Headers
+#' Returns the include headers required to compile code that uses the TileDB-VCF library.
+#' @return A character string containing the include headers.
+#' @export
+tiledb_vcf_include_headers <- function() {
+  include_path <- tiledb_vcf_include_path()
+  headers <- list.files(include_path, full.names = TRUE, recursive = TRUE)
+  headers <- headers[grepl("\\.h$", headers)] |>
+    normalizePath() |>
+    paste0(collapse = " ")
+  return(headers)
+}
+
+#' Get TileDB-VCF Compilation Flags
+#' Returns the compilation flags required to compile code that uses the TileDB-VCF library.
+#' @return A character string containing the compilation flags.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Example of using tiledb_vcf_pkg_cflags in a compilation command
+#' system(paste("R CMD SHLIB -o my_program.so my_program.c", tiledb_vcf_pkg_lflags(), tiledb_vcf_pkg_cflags()))
+#' }
+tiledb_vcf_pkg_cflags <- function() {
+  libs <- tiledb_vcf_library_items()
+  includes <- tiledb_vcf_include_headers()
+  pkg_cflags <- paste0("-I", tiledb_vcf_include_path())
+  return(pkg_cflags)
+}
+
+#' Get TileDB-VCF Linking Flags
+#'' Returns the linking flags required to link against the TileDB-VCF library.
+#'' @return A character string containing the linking flags.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Example of using tiledb_vcf_pkg_lflags in a compilation command
+#' system(paste("R CMD SHLIB -o my_program.so my_program.c", tiledb_vcf_pkg_lflags(), tiledb_vcf_pkg_cflags()))
+#' }
+#'
+tiledb_vcf_pkg_lflags <- function() {
+  libs <- tiledb_vcf_library_items()
+  lflags <- paste0("-L", tiledb_vcf_library_path(), " -l:", libs)
+  return(lflags)
+}
